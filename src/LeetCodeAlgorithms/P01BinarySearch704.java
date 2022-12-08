b4a26a792a873e99f1a54638160a598dbdf8e314
Niki <nickkolev06@abv.bg>
2022-12-08 17:15:02 +0200

    Started learning algorithms on LeetCode

---
+++ src/LeetCodeAlgorithms/P01BinarySearch704.java
@@ -0,0 +1,29 @@
+package LeetCodeAlgorithms;
+
+public class P01BinarySearch704 {
+    public static void main(String[] args) {
+
+        int result1 = search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
+        int result2 = search(new int[] {-1,0,3,5,9,12}, 2);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
