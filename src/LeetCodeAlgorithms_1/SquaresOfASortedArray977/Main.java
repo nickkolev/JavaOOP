0339dde12e82e690df6e6cbe71836f8f30f86b5c
Niki <nickkolev06@abv.bg>
2023-01-04 22:22:38 +0200

    Optimized solution -> Two pointer solution

--- src/LeetCodeAlgorithms_1/SquaresOfASortedArray977/Main.java
+++ src/LeetCodeAlgorithms_1/SquaresOfASortedArray977/Main.java
@@ -14,10 +14,37 @@ public class Main {
     public static int[] sortedSquares(int[] nums) {
 
         // Brute force
+
+        /*
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
         }
         Arrays.sort(nums);
         return nums;
+         */
+
+        // Optimized solution - O(n)
+        // Input: nums = [-7,-3,2,3,11]
+        // the elements with their absolute value -> the element at the start and the last can be the highest value.
+        // We can use the Two pointer solution
+
+        int left = 0;
+        int right = nums.length -1;
+        int index = nums.length- 1;
+        int result[] = new int [nums.length];
+
+        while(left <= right) {
+            {
+                if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                    result[index] = nums[left] * nums[left];
+                    left++;
+                } else {
+                    result[index] = nums[right] * nums[right];
+                    right--;
+                }
+                index--;
+            }
+        }
+        return result;
     }
 }
